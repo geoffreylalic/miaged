e090b2cebec9d35218df24c82b512eb6f85e14a8
Geoffrey <geoffrey0151@gmail.com>
2023-02-21 14:28:16 +0100

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:miaged/firebase_options.dart';
 import 'package:miaged/pages/authentication/login.dart';
 import 'package:miaged/pages/authentication/register.dart';
 import 'package:miaged/pages/clothingList.dart';
@@ -7,7 +9,9 @@ import 'package:miaged/pages/home.dart';
 import 'package:miaged/pages/search.dart';
 import 'package:miaged/widgets/customButtomAppBar.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
